a8780a2e15cfb38a49a5867dbeb01b468f071ee1
aadil124 <ansariaadil124@gmail.com>
2023-08-24 20:47:23 +0530

    add react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from "./components/Homepage/HomePage";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  const [userLogin, setUserLogin] = useState({
+    email: "",
+    password: "",
+  });
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          userLogin && userLogin._id ? (
+            <HomePage setUserLogin={setUserLogin} />
+          ) : (
+            <Login setUserLogin={setUserLogin} />
+          )
+        }
+      />
+      <Route path="/login" element={<Login setUserLogin={setUserLogin} />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 };
 
 export default App;

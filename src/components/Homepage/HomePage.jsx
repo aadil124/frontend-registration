bfc21d545d5af7980bf48e72e46ea235acd5c441
aadil124 <ansariaadil124@gmail.com>
2023-08-24 20:46:32 +0530

    add home page

--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio et,
+        perspiciatis repellendus reiciendis sint possimus ipsa aspernatur
+        similique minus tenetur eligendi odio? Ut, ratione exercitationem?
+        Architecto voluptate laudantium id aperiam! Doloremque voluptas ipsa eum
+        quia illo molestiae, non enim libero veritatis reprehenderit! Facere
+        doloribus sunt facilis aut consequatur illum minus rerum, adipisci
+        tempore cumque, nobis maiores odio sapiente ipsam voluptates. Et labore
+        molestias amet adipisci harum hic fugit neque veniam accusamus ea enim
+        sequi sed, illum expedita culpa dolorum, quam iure officia. Non
+        laudantium tenetur pariatur fugiat? Ex, facere tempora. Cupiditate
+        mollitia voluptatem temporibus architecto laboriosam, distinctio
+        repudiandae rerum voluptas? In architecto ducimus, quo ad ipsa esse
+        facilis! Tempore facere excepturi quidem atque accusamus nemo beatae
+        officia tenetur deleniti quasi? Autem asperiores tempora ducimus
+        perferendis possimus cumque incidunt, ullam quia recusandae. Debitis,
+        fugiat, tempora dolorem dolore possimus corporis est voluptatum
+        aspernatur repudiandae inventore error impedit, quam ducimus id corrupti
+        autem. Provident doloremque consequuntur porro, illum animi laborum
+        error pariatur expedita amet harum ad, cumque saepe quas repellendus
+        itaque soluta earum nulla tempore impedit ducimus possimus aut maxime.
+        Saepe, unde accusamus! Impedit assumenda sequi quidem ratione
+        aspernatur! Eius ex culpa sapiente rem illo ipsam at placeat quae, in
+        eos perspiciatis assumenda maiores architecto, dolores deserunt dolorum.
+        Necessitatibus natus facilis autem nostrum! Saepe eveniet dolor, ullam
+        accusamus nam quas itaque mollitia odio libero dolores consequuntur
+        deleniti, consectetur, eius consequatur aut tenetur iste laudantium
+        ratione ipsum rem minima reprehenderit ducimus animi. Molestiae,
+        inventore?
+      </p>
+      <Button onClick={() => navigate("/login")}>Logout</Button>
+    </div>
+  );
 };
 
 export default HomePage;

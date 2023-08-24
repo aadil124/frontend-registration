b6cff6b664af66054e425e2da3f64f12622245a5
aadil124 <ansariaadil124@gmail.com>
2023-08-24 20:46:49 +0530

    add login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ setUserLogin }) => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((user) => ({ ...user, [name]: value }));
+  };
+  const handleLogin = async () => {
+    await axios
+      .post("http://localhost:9000/login", user)
+      .then((res) => {
+        alert(res.data.message);
+        //   console.log(res.data.user);
+        setUserLogin(res.data.user);
+      })
+      .catch((err) => console.log(err));
+    navigate("/");
+  };
+
+  const handleRegister = () => {
+    navigate("/register");
+  };
+
+  return (
+    <>
+      <div className="Form">
+        <h1>Login</h1>
+        <div className="content">
+          <input
+            type="email"
+            name="email"
+            value={user.email}
+            placeholder="Enter your email"
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={user.password}
+            placeholder="Enter your password"
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <Button className="me-3" onClick={handleLogin}>
+            Login
+          </Button>
+
+          <Button onClick={handleRegister}>Registration</Button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;

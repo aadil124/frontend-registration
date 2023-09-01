ab276a8d69359cb151dd0b1d0e8b405fdad778ee
aadil124 <ansariaadil124@gmail.com>
2023-09-01 20:26:43 +0530

    updated homepage

--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -1,38 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
+// import Login from "../Login/Login.js";
 import { useNavigate } from "react-router-dom";
 
-const HomePage = () => {
+const Homepage = ({ setLoginUser, loginUser }) => {
   const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setLoginUser(localStorage.removeItem("jwt-token")); //remove the token from local storage and update the
+    // state to logout the user
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    if (!loginUser) {
+      navigate("/login");
+    }
+  }, [loginUser, navigate]);
+  console.log(loginUser);
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio et,
-        perspiciatis repellendus reiciendis sint possimus ipsa aspernatur
-        similique minus tenetur eligendi odio? Ut, ratione exercitationem?
-        Architecto voluptate laudantium id aperiam! Doloremque voluptas ipsa eum
-        quia illo molestiae, non enim libero veritatis reprehenderit! Facere
-        doloribus sunt facilis aut consequatur illum minus rerum, adipisci
-        tempore cumque, nobis maiores odio sapiente ipsam voluptates. Et labore
-        molestias amet adipisci harum hic fugit neque veniam accusamus ea enim
-        sequi sed, illum expedita culpa dolorum, quam iure officia. Non
-        laudantium tenetur pariatur fugiat? Ex, facere tempora. Cupiditate
-        mollitia voluptatem temporibus architecto laboriosam, distinctio
-        repudiandae rerum voluptas? In architecto ducimus, quo ad ipsa esse
-        facilis! Tempore facere excepturi quidem atque accusamus nemo beatae
-        officia tenetur deleniti quasi? Autem asperiores tempora ducimus
-        perferendis possimus cumque incidunt, ullam quia recusandae. Debitis,
-        fugiat, tempora dolorem dolore possimus corporis est voluptatum
-        aspernatur repudiandae inventore error impedit, quam ducimus id corrupti
-        autem. Provident doloremque consequuntur porro, illum animi laborum
-        error pariatur expedita amet harum ad, cumque saepe quas repellendus
-        itaque soluta earum nulla tempore impedit ducimus possimus aut maxime.
-        Saepe, unde accusamus! Impedit assumenda sequi quidem ratione
-      </p>
-      <Button onClick={() => navigate("/login")}>Logout</Button>
-    </div>
+    <>
+      <div>
+        <h1>This is Homepage</h1>
+      </div>
+      <Button onClick={() => handleLogout()}>Logout</Button>
+    </>
   );
 };
 
-export default HomePage;
+export default Homepage;
